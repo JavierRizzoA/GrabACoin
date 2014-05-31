1cbd60fec8e37ca7b02679e351b2c78ace447cda
Javier Rizzo <javierrizzoa@gmail.com>
2014-05-31 12:21:52 -0700

    Add dialog

--- src/MainScene.hx
+++ src/MainScene.hx
@@ -3,6 +3,8 @@ import com.haxepunk.Scene;
 import com.haxepunk.tmx.TmxEntity;
 import com.haxepunk.tmx.TmxMap;
 import com.haxepunk.HXP;
+import com.haxepunk.graphics.Text;
+
 
 class MainScene extends Scene
 {
@@ -27,6 +29,17 @@ class MainScene extends Scene
 		for(object in map.getObjectGroup("Coins").objects) {
 			add(new entities.Coin(object.x, object.y));
 		}
+
+		addGraphic(new Text("Seriously you can't win?", 46*32, 76*32, 0, 0, {size: 32}));
+		addGraphic(new Text("This is like the easiest game ever...", 46*32, 77*32, 0, 0, {size: 32}));
+		addGraphic(new Text("You're a failure in life.", 73*32, 67*32, 0, 0, {size: 32}));
+		addGraphic(new Text("Here, have some coins and win. Please.", 106*32, 66*32, 0, 0, {size: 32}));
+		addGraphic(new Text("So you want to loose, huh?", 135*32, 67*32, 0, 0, {size: 32}));
+		addGraphic(new Text("Ok...", 167*32, 66*32, 0, 0, {size: 32}));
+		addGraphic(new Text("You'll have to kill yourself.", 173*32, 66*32, 0, 0, {size: 32}));
+		addGraphic(new Text("But you can't.", 177*32, 86*32, 0, 0, {size: 32}));
+		addGraphic(new Text("You can just win.", 177*32, 87*32, 0, 0, {size: 32}));
+
 	}
 
 	private function adjustCamera() {
